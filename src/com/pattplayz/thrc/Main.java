28a13b8b8baafb552a8dd880dc65daaf5cd36cac
BuildTools <unconfigured@null.spigotmc.org>
2022-04-04 22:43:36 -0400

    Added main functionality with prompts

--- src/com/pattplayz/thrc/Main.java
+++ src/com/pattplayz/thrc/Main.java
@@ -1,8 +1,31 @@
 package com.pattplayz.thrc;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the following information:");
 
+        System.out.println("Please enter your first name:");
+        String firstName = input.next();
+
+        System.out.println("Please enter your last name:");
+        String lastName = input.next();
+
+        System.out.println("Please enter your year of birth (yyyy):");
+        int dobYear = input.nextInt();
+
+        System.out.println("Please enter your month of birth (mm):");
+        int dobMonth = input.nextInt();
+
+        System.out.println("Please enter your day of birth (dd):");
+        int dobDay = input.nextInt();
+
+        System.out.println("Calculating target heart rate.....");
+
+        HeartRates user = new HeartRates(firstName, lastName, dobMonth, dobDay, dobYear);
+        System.out.println("Your recommended target heart rate is " + user.calculateTargetHR() + " bpm.");
     }
 }
